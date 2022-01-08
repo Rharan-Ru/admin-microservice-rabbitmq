474d7b70d1be4adbcf77ec3eb10d69f8d25a3c9e
Monke001 <rharanru@gmail.com>
2022-01-08 02:05:31 -0300

    Add consumer to save likes from main microservice

--- consumer.py
+++ consumer.py
@@ -1,19 +1,27 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admin.settings")
+import django
+django.setup()
 import json
 import pika
-
-
+from products.models import Product
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmqhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='admin')
 
 
 def callback(ch, method, properties, body):
     data = json.loads(body)
-    print(f"Main Received {data}")
+    print(f"Admin Received {data}")
+    if properties.content_type == 'product_created':
+        product = Product.objects.get(pk=data)
+        product.likes += 1
+        product.save()
+        print('Product liked')
 
 
-channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
 channel.close()
--- products/producer.py
+++ products/producer.py
@@ -5,10 +5,10 @@ connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='rabbitmqhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='admin')
 
 
 def publish(method, body):
     properties = pika.BasicProperties(method)
-    channel.basic_publish(exchange='', routing_key='hello', body=json.dumps(body), properties=properties)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
     print(f"Send {body}")
